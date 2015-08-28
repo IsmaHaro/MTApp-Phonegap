6e514eadf422bd3196edd9983ddb0a2ec9adc03b
Isma <ismaharo>
2015-08-28 01:21:15 -0500

    Mejoras en js

--- js/API/mediaCapture.js
+++ js/API/mediaCapture.js
@@ -1,15 +1,16 @@
 var mc = {
     captureSuccess: function(mediaFiles) {
-        var i, path, len;
-        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-            path = mediaFiles[i].fullPath;
+        // var i, path, len;
+        // for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        //     path = mediaFiles[i].fullPath;
 
             // desplegar la foto tomada
-            fn.cambiarPagina("#fotoProceso");
-            $('#fotoTomada').css('background-image', 'url("'+path+'")');
-            $('#fotoTomada').attr("rel", path);
+
+        fn.cambiarPagina("#fotoProceso");
+        $('#fotoTomada').css('background-image', 'url("'+mediaFiles[0].fullPath+'")');
+        $('#fotoTomada').attr("rel", mediaFiles[0].fullPath);
             
-        }
+        // }
     },
     captureError: function(error) {
         navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
